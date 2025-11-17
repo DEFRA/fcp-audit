6e0fbf1d7665322088573acaf2b01d95190a890e
John Watson <john.watson1@defra.gov.uk>
2025-11-17 10:21:59 +0000

    Add auditing

---
+++ src/events/soc.js
@@ -0,0 +1,8 @@
+import { audit } from '@defra/cdp-auditing'
+import { config } from '../config/config.js'
+
+export function sentToSoc (event) {
+  if (config.get('soc.enabled')) {
+    audit(event)
+  }
+}
