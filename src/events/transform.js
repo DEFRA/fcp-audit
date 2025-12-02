a403adb94b8de3302ec1f3b46ff89d88109ff59f
John Watson <john.watson1@defra.gov.uk>
2025-12-02 14:41:47 +0000

    Update transform

--- src/events/transform.js
+++ src/events/transform.js
@@ -1,40 +1,36 @@
 export function transformEvent (event) {
   return {
-    auditEvent: createAuditEvent(event),
-    socEvent: createSocEvent(event)
+    auditEvent: event.audit && createAuditEvent(event),
+    socEvent: event.security && createSocEvent(event)
   }
 }
 
 function createAuditEvent (event) {
-  if (event.audit) {
-    return {
-      user: event.user,
-      sessionid: event.sessionid,
-      datetime: event.datetime,
-      environment: event.environment,
-      version: event.version,
-      application: event.application,
-      component: event.component,
-      ip: event.ip,
-      audit: { ...event.audit }
-    }
+  return {
+    user: event.user,
+    sessionid: event.sessionid,
+    datetime: event.datetime,
+    environment: event.environment,
+    version: event.version,
+    application: event.application,
+    component: event.component,
+    ip: event.ip,
+    audit: { ...event.audit }
   }
 }
 
 function createSocEvent (event) {
-  if (event.security) {
-    return {
-      user: event.user,
-      sessionid: event.sessionid,
-      datetime: event.datetime,
-      environment: event.environment,
-      version: event.version,
-      application: event.application,
-      component: event.component,
-      ip: event.ip,
-      pmcode: event.security.pmcode,
-      priority: event.security.priority,
-      details: { ...event.security.details },
-    }
+  return {
+    user: event.user,
+    sessionid: event.sessionid,
+    datetime: event.datetime,
+    environment: event.environment,
+    version: event.version,
+    application: event.application,
+    component: event.component,
+    ip: event.ip,
+    pmcode: event.security.pmcode,
+    priority: event.security.priority,
+    details: { ...event.security.details },
   }
 }
