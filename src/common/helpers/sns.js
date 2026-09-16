05c8a6296c95bbb9e44ca0ee2ed828d78a068493
David Cohen <david.cohen@defra.gov.uk>
2026-09-16 09:39:58 +0100

    chore: removed sns/sqs client constructor params
    * chore: removed sns/sqs client constructor params as the environment variable names map directly to these settings

--- src/common/helpers/sns.js
+++ src/common/helpers/sns.js
@@ -1,14 +1,5 @@
 import { SNSClient } from '@aws-sdk/client-sns'
-import { config } from '../../config/config.js'
 
-const { region, endpoint, accessKeyId, secretAccessKey } = config.get('aws')
-
-const snsClient = new SNSClient({
-  region,
-  ...(endpoint && {
-    endpoint,
-    credentials: { accessKeyId, secretAccessKey }
-  })
-})
+const snsClient = new SNSClient()
 
 export { snsClient }
